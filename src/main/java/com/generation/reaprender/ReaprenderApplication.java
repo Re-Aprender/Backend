05892af3c8e6c83cd0e228e90ea5f9ddb7bb0f59
Lucas Barbosa <lucasbarbosa0217@gmail.com>
2024-07-05 11:30:45 -0300

    Innitial Commit

---
+++ src/main/java/com/generation/reaprender/ReaprenderApplication.java
@@ -0,0 +1,13 @@
+package com.generation.reaprender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaprenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaprenderApplication.class, args);
+	}
+
+}
